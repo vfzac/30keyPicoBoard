e9f53d00cf4c2b22f850e90215c35492f9930149
vfzac <vfzac@gmail.com>
2022-10-30 00:34:41 +0800

    added some modiiers(unfinished)

--- 30kbo.py
+++ 30kbo.py
@@ -8,9 +8,15 @@ from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.modules.layers import Layers
 from kmk.scanners import DiodeOrientation
+from kmk.modules.tapdance import TapDance
+from kmk.handlers.sequences import simple_key_sequence
+
+
 
 keyboard = _KMKKeyboard()
+tapdance = TapDance()
 keyboard.modules.append(Layers())
+keyboard.modules.append(tapdance)
 
 keyboard.col_pins = (
     board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,
@@ -19,8 +25,38 @@ keyboard.col_pins = (
 keyboard.row_pins = (board.GP11, board.GP12, board.GP13, board.GP14)
 keyboard.diode_orientation = DiodeOrientation.COLUMNS
 
-
+# Additional Keys definition
 FN = KC.MO(1)
+FN2 = KC.MO(2)
+ALEFT = KC.TD(
+    KC.LCTRL,
+    FN,
+    FN2,
+)
+ARIGHT = KC.TD(
+    FN,
+    KC.LSHIFT,
+    FN2,
+)
+ALTTAB = simple_key_sequence(
+    (
+        KC.LALT(no_release=True), 
+        KC.MACRO_SLEEP_MS(30),
+        KC.TAB,
+        KC.MACRO_SLEEP_MS(30),
+        KC.LALT(no_press=True),
+    )
+)
+# CTLTAB = simple_key_sequence(
+#     (
+#         KC.LCTL(no_release=True), 
+#         KC.MACRO_SLEEP_MS(30),
+#         KC.TAB,
+#         KC.MACRO_SLEEP_MS(30),
+#         KC.LCTL(no_press=True),
+#     )
+# )
+
 XXXXXXX = KC.TRNS
 OOOOOOO = KC.NO
 
@@ -33,32 +69,50 @@ keyboard.keymap = [
     # |------+------+------+------+------+------+------+------+------+------|
     # |   Z  |   X  |   C  |   V  |   B  |   N  |   M  | Enter|______|______|
     # |------+------+------+------+------+------+------+------+------+------|
-    # |______|______|______| Ctrl |Space |  Fn  |______|______|______|______|
+    # |______|______|______|ALEFT |Space |ARIGHT|______|______|______|______|
     # '---------------------------------------------------------------------'
     [
         KC.Q,       KC.W,    KC.E,    KC.R,    KC.T,  KC.Y,    KC.U,      KC.I,    KC.O,    KC.P,
         KC.A,       KC.S,    KC.D,    KC.F,    KC.G,  KC.H,    KC.J,      KC.K,    KC.L, OOOOOOO,
         KC.Z,       KC.X,    KC.C,    KC.V,    KC.B,  KC.N,    KC.M,  KC.ENTER, OOOOOOO, OOOOOOO,
-        OOOOOOO, OOOOOOO, OOOOOOO, KC.LCTL,  KC.SPC,    FN, OOOOOOO,   OOOOOOO, OOOOOOO, OOOOOOO
+        OOOOOOO, OOOOOOO, OOOOOOO,   ALEFT,  KC.SPC,  ARIGHT, OOOOOOO,   OOOOOOO, OOOOOOO, OOOOOOO
     ],
     # FN
     # .---------------------------------------------------------------------.
-    # | ESC  |   7  |   8  |   9  |      |      |      |      |      | BSPC |
+    # | ESC  |     |     |     |      |      |      |      |      | BSPC |
     # |------+------+------+------+------+------+------+------+------+------|
-    # | TAB  |   4  |   5  |   6  |      |      |      |      |      |______|
+    # |ALTTAB|     |     |     |      |      |      |      |      |______|
     # |------+------+------+------+------+------+------+------+------+------|
-    # |      |   1  |   2  |   3  |   0  |   ,  |   .  | Enter|______|______|
+    # |CTLTAB|     |     |     |      |   ,  |   .  | Enter|______|______|
     # |------+------+------+------+------+------+------+------+------+------|
-    # |______|______|______| Alt  |Space |  Fn  |______|______|______|______|
+    # |______|______|______|ALEFT |Space |ARIGHT|______|______|______|______|
     # '---------------------------------------------------------------------'
     [  
-        KC.GESC,   KC.N7,   KC.N8,   KC.N9,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.BSPC,
-        KC.TAB,    KC.N4,   KC.N5,   KC.N6,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  OOOOOOO,
-        XXXXXXX,   KC.N1,   KC.N2,   KC.N3,    KC.N0, KC.COMM,  KC.DOT, KC.ENTER,  OOOOOOO,  OOOOOOO,
-        OOOOOOO, OOOOOOO, OOOOOOO, KC.LALT,   KC.SPC,      FN, OOOOOOO,  OOOOOOO,  OOOOOOO,  OOOOOOO
+        KC.GESC,    KC.N7,     KC.N8,     KC.N9,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.BSPC,
+        ALTTAB,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  OOOOOOO,
+        CTLTAB,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX, KC.COMM,  KC.DOT, KC.ENTER,  OOOOOOO,  OOOOOOO,
+        OOOOOOO, OOOOOOO, OOOOOOO, XXXXXXX,   KC.SPC, XXXXXXX, OOOOOOO,  OOOOOOO,  OOOOOOO,  OOOOOOO
+    ],
+    # FN2
+    # .---------------------------------------------------------------------.
+    # |   1   |   2  |   3  |   4  |  5   |  6   |  7   |  8   |  9   |  0  |
+    # |------+------+------+------+------+------+------+------+------+------|
+    # |XXXXXXX|      |      |      |      |      |      |      |      |_____|
+    # |------+------+------+------+------+------+------+------+------+------|
+    # |XXXXXXX|      |      |      |      |   ,  |   .  | Enter|______|_____|
+    # |------+------+------+------+------+------+------+------+------+------|
+    # |______ |______|______|ALEFT |Space |ARIGHT|______|______|______|_____|
+    # '---------------------------------------------------------------------'
+    [  
+        KC.N1,      KC.N2,    KC.N3,    KC.N4,    KC.N5,    KC.N6,    KC.N7,     KC.N8,    KC.N9,    KC.N0,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  OOOOOOO,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.COMM,   KC.DOT,  KC.ENTER,  OOOOOOO,  OOOOOOO,
+        OOOOOOO,  OOOOOOO,  OOOOOOO,  XXXXXXX,   KC.SPC,  XXXXXXX,  OOOOOOO,   OOOOOOO,  OOOOOOO,  OOOOOOO
     ],
 ]
 
 if __name__ == '__main__':
     keyboard.go()
+
+
 
